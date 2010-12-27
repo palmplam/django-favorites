990db77ba3fc311e9e52f0b57d544693ddea36bf
alek <alek@geralt.(none)>
2010-12-27 21:55:54 +0200

    Fixed bug and added tag to retrieve favorite from generated dict.

--- favorites/templatetags/favorite_tags.py
+++ favorites/templatetags/favorite_tags.py
@@ -57,6 +57,42 @@ def do_favorites_for_objects(parser, token):
         raise TemplateSyntaxError(_('%s tag requires exactly four arguments') % bits[0])
     if bits[3] != 'as':
         raise TemplateSyntaxError(_("third argument to %s tag must be 'as'") % bits[0])
-    return FavoritesForObjectsNode(bits[1], bits[2], bits[3])
+    return FavoritesForObjectsNode(bits[1], bits[2], bits[4])
 register.tag('favorites_for_objects', do_favorites_for_objects)
     
+class FavoriteEntryForItemNode(template.Node):
+    def __init__(self, item, dictionary, context_var):
+        self.item = item
+        self.dictionary = dictionary
+        self.context_var = context_var
+
+    def render(self, context):
+        try:
+            dictionary = template.resolve_variable(self.dictionary, context)
+            item = template.resolve_variable(self.item, context)
+        except template.VariableDoesNotExist:
+            return ''
+        context[self.context_var] = dictionary.get("%s" % item.id, None)
+        return ''
+
+def do_favorite_entry_for_item(parser, token):
+    """
+    Given an object and a dictionary keyed with object ids - as
+    returned by the ``favorites_for_objects`` template tags - 
+    retrieves the value for the given object and
+    stores it in a context variable, storing ``None`` if no value
+    exists for the given object.
+
+    Example usage::
+
+        {% favorite_entry_for_item object from favorites_dict as favorite %}
+    """
+    bits = token.contents.split()
+    if len(bits) != 6:
+        raise template.TemplateSyntaxError("'%s' tag takes exactly five arguments" % bits[0])
+    if bits[2] != 'from':
+        raise template.TemplateSyntaxError("second argument to '%s' tag must be 'from'" % bits[0])
+    if bits[4] != 'as':
+        raise template.TemplateSyntaxError("fourth argument to '%s' tag must be 'as'" % bits[0])
+    return FavoriteEntryForItemNode(bits[1], bits[3], bits[5])
+register.tag('favorite_entry_for_item', do_favorite_entry_for_item)
