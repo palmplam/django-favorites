e374821b5662c69cbc1e32ac7d5dd0159e46a30b
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-06-20 18:03:03 +0200

    pep8

--- favorites/urls.py
+++ favorites/urls.py
@@ -3,9 +3,9 @@ from django.views.generic.simple import direct_to_template
 from views import *
 
 urlpatterns = patterns("",
-    url(r'^add$', ajax_add_favorite, name="favorite_ajax_add"),
-    url(r'^remove$', ajax_remove_favorite, name="favorite_ajax_remove"),
-    url(r'^delete/(?P<object_id>\d+)/$', drop_favorite, name="favorite_drop"),
-    url(r'^toggle/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
-            toggle_favorite, name="favorite_toggle"),
-)
+                       url(r'^add$', ajax_add_favorite, name="favorite_ajax_add"),
+                       url(r'^remove$', ajax_remove_favorite, name="favorite_ajax_remove"),
+                       url(r'^delete/(?P<object_id>\d+)/$', drop_favorite, name="favorite_drop"),
+                       url(r'^toggle/(?P<content_type_id>\d+)/(?P<object_id>\d+)/$',
+                           toggle_favorite, name="favorite_toggle"),
+                       )
