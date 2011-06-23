718ccdb2bf19276236fa9693bacae57bcd8bb5ca
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-06-23 12:35:56 +0200

    starting tests

--- favorites/tests.py
+++ favorites/tests.py
@@ -1,73 +1,51 @@
-import unittest
-
+from django.utils import unittest
+from django.test import Client
 from django.db import models
 from django.contrib.auth.models import User
 
 from models import Favorite
+from models import Folder
+
 from managers import FavoritesManagerMixin
 
-class BaseFavoriteTestCase(unittest.TestCase):
+
+class DummyModel(models.Model):
+    pass
+
+
+class BaseFavoritesTestCase(unittest.TestCase):
     def setUp(self):
-        self.users = dict([(u.username, u) for u in User.objects.all()])
-        if len(self.users) != 4:
-            for name in ['alice', 'bob', 'chris', 'dawn']:
-                try:
-                    u = User.objects.create(username=name)
-                    self.users[name] = u
-                except:
-                    pass
+        self.users = dict()
+        for name in ['godzilla', 'leviathan']:
+            u = User.objects.create(username=name)
+            u.set_password(name)
+            self.users[name] = u  # password is same as login
+        self.client = Client(enforce_csrf_checks=True)
 
-class FavoriteTestCase(BaseFavoriteTestCase):
-    def testAddFavorite(self):
-        """ Add alice as a favorite of chris, add the favorite as a favorite 
-        of alice 
-        """
-        chris = self.users['chris']
-        alice = self.users['alice']
-        favorite = Favorite.objects.create_favorite(chris, alice)
 
-        self.assertEquals(favorite.user, chris)        
-        self.assertEquals(favorite.content_object, alice)
+class FolderTests(BaseFavoritesTestCase):
+    def setUp(self):
+        super(FolderTests, self).setUp()
 
-        meta_favorite = Favorite.objects.create_favorite(alice, favorite)
+        godzilla = self.users['godzilla']
 
-        self.assertEquals(meta_favorite.user, alice)
-        self.assertEquals(meta_favorite.content_object, favorite)
+        Folder(name="foo", user=godzilla).save()
+        Folder(name="bar", user=godzilla).save()
 
-    def testGetFavoritesForUser(self):
-        """ Get favorites for chris """
-        chris = self.users['chris']
-        alice = self.users['alice']
 
-        # verify that people can get them
-        favorites = Favorite.objects.favorites_for_user(chris)
+    def testFolders(self):
+        reponse = self.client.login('godzilla')
+        response = self.client.get('/folders')
+        object_list = response.context['object_list']
 
-        self.assertEquals(len(favorites), 1)
-        self.assertEquals(favorites[0].content_object, self.users['alice'])
-        
-    def testGetFavoritesForModel(self):
-        alice = self.users['alice']
+        for folder in user_folders:
+            self.assertTrue(folder.name in ['foo', 'bar'])
 
-        # the meta favorite
-        meta_favorite = Favorite.objects.get(pk=2)
 
-        favorites = Favorite.objects.favorites_for_model(Favorite)
-
-        self.assertEquals(len(favorites), 1)
-        self.assertEquals(favorites[0], meta_favorite)
-        self.assertEquals(favorites[0].user, alice)
-
-    def testGetFavoritesForObject(self):
-        alice = self.users['alice']
-
-        favorite = Favorite.objects.get(pk=1)
-
-        favorites = Favorite.objects.favorites_for_object(favorite)
-        self.assertEquals(len(favorites), 1)
-        self.assertEquals(favorites[0].user, alice)
-
+"""
 class AnimalManager(models.Manager, FavoritesManagerMixin):
     pass
+
 
 class Animal(models.Model):
     name = models.CharField(max_length=20)
@@ -79,8 +57,6 @@ class Animal(models.Model):
 
 class FavoritesMixinTestCase(BaseFavoriteTestCase):
     def testWithFavorites(self):
-        """ Tests whether or not `with_favorite_for` works
-        """
         alice = self.users['alice']
         chris = self.users['chris']
         animals = {}
@@ -100,3 +76,4 @@ class FavoritesMixinTestCase(BaseFavoriteTestCase):
 
         favorite_animals = Animal.objects.with_favorite_for(alice, all=False).all()
         self.assertEquals(len(favorite_animals), 1)
+"""
