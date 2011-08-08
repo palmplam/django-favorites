ce50042f364cb4b8286a28ef81f63f25931494d4
Amirouche Boubekki <amiroucheb@pilotsystems.net>
2011-08-08 18:08:07 +0200

    added pep8 cr

--- favorites/urls.py
+++ favorites/urls.py
@@ -6,6 +6,7 @@ from django.views.generic.list import ListView
 from favorites.models import Favorite
 from favorites.models import Folder
 
+
 urlpatterns = patterns("",
                        # favorites urls
                        url(r'^favorites/$',
